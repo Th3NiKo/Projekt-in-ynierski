0ac9ce0104659d58a316f9e3c8a79ae2e176e498
Th3NiKo <niko.pachala@onet.eu>
2018-06-05 20:41:06 +0200

    Downloaded files in other folder

--- Assets/Scripts/Flying/GenerateInfinite.cs
+++ Assets/Scripts/Flying/GenerateInfinite.cs
@@ -22,8 +22,8 @@ public class GenerateInfinite : MonoBehaviour {
     public GameObject player;
 
     int planeSize = 10;
-    int halfTilesX = 10;
-    int halfTilesZ = 10;
+    int halfTilesX = 5;
+    int halfTilesZ = 7;
 
     Vector3 startPos;
 
@@ -40,7 +40,7 @@ public class GenerateInfinite : MonoBehaviour {
         {
             for (int z = -halfTilesZ; z < halfTilesZ; z++)
             {
-                Vector3 pos = new Vector3((x * planeSize + startPos.x), 0, (z * planeSize + startPos.z));
+                Vector3 pos = new Vector3((x * planeSize + startPos.x), -5, (z * planeSize + startPos.z));
                 GameObject t = (GameObject)Instantiate(plane, pos, Quaternion.identity);
 
                 string tilename = "Tile_" + ((int)(pos.x)).ToString() + "_" + ((int)(pos.z)).ToString();
@@ -54,7 +54,53 @@ public class GenerateInfinite : MonoBehaviour {
  }
  
  
- void Update () {
-  
+ void FixedUpdate () {
+     int xMove = (int)(player.transform.position.x - startPos.x);
+     int zMove = (int)(player.transform.position.z - startPos.z);
+
+     if(Mathf.Abs(xMove) >= planeSize || Mathf.Abs(zMove) >= planeSize){
+         float updateTime = Time.realtimeSinceStartup;
+
+         int playerX = (int)(Mathf.Floor(player.transform.position.x/planeSize)*planeSize);
+         int playerZ = (int)(Mathf.Floor(player.transform.position.z/planeSize)*planeSize);
+
+         for(int x = -halfTilesX;x < halfTilesX; x++){
+             for(int z = -halfTilesZ; z <halfTilesZ; z++){
+                 Vector3 pos = new Vector3((x * planeSize + playerX),-5,(z * planeSize + playerZ));
+                 string tilename = "Tile_" + ((int)(pos.x)).ToString() + "_" + ((int)(pos.z)).ToString();
+
+                 if(!tiles.ContainsKey(tilename)){
+                     GameObject t = (GameObject) Instantiate(plane, pos, Quaternion.identity);
+                     t.name = tilename;
+                     Tile tile = new Tile(t, updateTime);
+                     tiles.Add(tilename, tile);
+                 } else {
+                     (tiles[tilename] as Tile).creationTime = updateTime;
+                 }
+             }
+         }
+
+
+        Hashtable newTerrain = new Hashtable();
+        foreach(Tile tls in tiles.Values){
+            if(tls.creationTime != updateTime){
+                Destroy(tls.theTile);
+            } else {
+                newTerrain.Add(tls.theTile, tls);
+            }
+        }
+        tiles = newTerrain;
+
+        startPos = player.transform.position;
+         
+     }
+
+
  }
+
+
+
+
+
+
 }
