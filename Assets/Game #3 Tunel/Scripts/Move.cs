f85878543fa65f9db31ef3d5b36c4455690912a1
Rafal Muzia <rafal.muzia@gmail.com>
2018-08-08 21:32:33 +0200

    tunel- rotate and move ohj

---
+++ Assets/Game #3 Tunel/Scripts/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	[SerializeField] Vector2 position;
+	[SerializeField] Vector3 angles;
+	[Range(0,50000)] public float sensitivity = 10000;
+	Rigidbody rgb;
+	MessageReceiver msg;
+	void Start () {
+		rgb = GetComponent<Rigidbody> ();
+		msg = GameObject.Find ("Controler").GetComponent<MessageReceiver>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		position = msg.LoadPositions ();
+		angles = msg.LoadAngles ();
+		rgb.velocity = new Vector3 (position.x / sensitivity, position.y / sensitivity, 0);
+		this.transform.position = new Vector3(Mathf.Clamp(this.transform.position.x,-0.33f, 0.33f), Mathf.Clamp(this.transform.position.y, -0.33f, 0.33f),0);
+	}
+}
---
+++ Assets/Game #3 Tunel/Scripts/Rotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	Material mat;
+	public float speed = 1.0f;
+	void Start () {
+		mat = GetComponent<Renderer>().material;
+	}
+	
+	
+	void FixedUpdate () {
+		Vector2 offset = mat.mainTextureOffset;
+		mat.mainTextureOffset = new Vector2(offset.x + Time.deltaTime * speed, offset.y + Time.deltaTime * speed);
+		Vector2 tiling = mat.mainTextureScale;
+		mat.mainTextureScale = new Vector2(Mathf.Cos(Time.timeSinceLevelLoad / 2) + 2, tiling.y);
+	}
+}
