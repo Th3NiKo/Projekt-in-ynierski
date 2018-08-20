1a145ea40fdc361d458a6faed65eac5e84caa3b2
drastiq <ziomus5555@gmail.com>
2018-08-20 20:11:19 +0100

    zliczanie punktow

---
+++ Assets/Game #3 Tunel/Scripts/MeterCount.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeterCount : MonoBehaviour {
+
+    public GUIText scoreText;
+    private float score;
+	Text meters;
+	void Start () {
+		meterCount = 0.0f;
+		meters = GetComponent<Text>();
+	}
+	
+	void Update () {
+		meterCount += Time.deltaTime * 15;
+		
+		meters.text = Mathf.CeilToInt(meterCount).ToString() + "m";
+	}
+	public void AddScore (int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore ();
+    }
+
+    void UpdateScore ()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
+}
