2ae19d7076638b8876bd94e7e050cbeb2940af23
drastiq <ziomus5555@gmail.com>
2018-08-27 20:11:19 +0100

    draw ui Add range

--- Assets/Game #3 Tunel/Scripts/UIDraw.cs
+++ Assets/Game #3 Tunel/Scripts/UIDraw.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UIDraw : MonoBehaviour {
+    [Range(5, 50)]
+    public int BlackbarPercentage = 20;
+    public bool ControllerOffset;
+    public bool InvertOffset = true;
 
-	public float angularSpeed = 100.0f;
+    [Range(0, 100)]
+    public int Sensitivity = 50;
+    public float XClampMax = 50;
+    public float YClampMax = 50;
+    public float angularSpeed = 100.0f;
     public void FixedUpdate()
     {
         RefreshUI();
