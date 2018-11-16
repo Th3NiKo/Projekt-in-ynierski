a4ee623a69b50f596d704d4fad1810e0d78c40aa
Rafal Muzia <rafal.muzia@gmail.com>
2018-11-16 20:15:10 +0200

    VR test

---
+++ Assets/Scripts/testVR/CVR.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraVR : MonoBehaviour {
+
+	public float angle = 5;
+	public float shift = 0.3f;
+    public GameObject cameraTemplate;
+	void Start () {
+        //Instantiate eyes
+        
+		GameObject leftEye = Instantiate(cameraTemplate);
+		GameObject rightEye = Instantiate(cameraTemplate);
+        leftEye.name = "LeftEye";
+        rightEye.name = "RightEye";
+
+        //Parent objects
+        leftEye.transform.SetParent(this.gameObject.transform);
+		rightEye.transform.SetParent(this.gameObject.transform);
+
+		//Turn Off camera on main
+		//this.GetComponent<Camera>().enabled = false;
+
+		//Move on X eyes
+		//If has camera look at 
+		if(leftEye.GetComponent<CameraLookAt>()){
+			leftEye.GetComponent<CameraLookAt>().offset.x -= shift;
+			rightEye.GetComponent<CameraLookAt>().offset.x += shift;
+		} else { 
+			//Without camera look at
+			leftEye.transform.position = new Vector3(leftEye.transform.position.x - shift, 
+													 leftEye.transform.position.y, 
+													 leftEye.transform.position.z);
+			rightEye.transform.position = new Vector3(rightEye.transform.position.x + shift, 
+													 rightEye.transform.position.y, 
+													 rightEye.transform.position.z);
+		}
+
+		//Change angle of eyes
+		leftEye.transform.rotation = Quaternion.Euler(leftEye.transform.rotation.eulerAngles.x,
+													  leftEye.transform.rotation.eulerAngles.y + angle,
+													  leftEye.transform.rotation.eulerAngles.z);
+		rightEye.transform.rotation = Quaternion.Euler(rightEye.transform.rotation.eulerAngles.x,
+													  rightEye.transform.rotation.eulerAngles.y - angle,
+													  rightEye.transform.rotation.eulerAngles.z);
+
+		//Split screens to see two eyes 
+		leftEye.GetComponent<Camera>().rect = new Rect(0,0,0.5f, 1.0f);
+		rightEye.GetComponent<Camera>().rect = new Rect(0.5f,0,0.5f, 1.0f);
+
+	}
+	
+	
+}
