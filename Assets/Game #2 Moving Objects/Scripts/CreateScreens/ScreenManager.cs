44f96a6f82a168ed842b4f2545af514e4fd587e3
unknown <niko.pachala@onet.eu>
2018-12-06 09:16:09 +0200

    Tworzenie ekranow

---
+++ Assets/Game #2 Moving Objects/Scripts/CreateScreens/ScreenManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenManager : MonoBehaviour
+{
+
+    MessageReceiver msg;
+    public GameObject photoPrefab;
+
+    void Start()
+    {
+        msg = GameObject.Find("Controler").GetComponent<MessageReceiver>();
+    }
+    void OnTriggerStay(Collider col)
+    {
+        if (col.name == "Cursor")
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || msg.ButtonPressedDown(0))
+            {
+                Quaternion rotationTemp = photoPrefab.transform.rotation;
+                Vector3 pos = new Vector3(0, 3, 3);
+                if (SceneManager.GetActiveScene().name == "VR")
+                {
+                    pos = new Vector3(-15.5f, -4.6f, 10.8f);
+
+                }
+                GameObject x = Instantiate(photoPrefab, pos, rotationTemp);
+                if (SceneManager.GetActiveScene().name == "VR")
+                {
+                    if (photoPrefab.name == "Speaker")
+                    {
+                        x.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                    }
+                    else
+                    {
+                        x.transform.localScale = new Vector3(0.04f, 0.025f, 0.04f);
+                    }
+                }
+            }
+        }
+    }
+}
