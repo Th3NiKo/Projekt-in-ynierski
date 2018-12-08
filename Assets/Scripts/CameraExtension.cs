03fb34ec156abccdec2bac956192b98df0c8584c
drastiq <ziomus5555@gmail.com>
2018-12-08 08:21:43 +0200

    EVEN MORE camera modes

--- Assets/Scripts/CameraExtension.cs
+++ Assets/Scripts/CameraExtension.cs
@@ -27,6 +27,37 @@ public static class CameraExtensions
     {
         cam.cullingMask ^= layerMask;
     }
+    public static void LayerCullingToggle(this Camera cam, string layer)
+    {
+        LayerCullingToggle(cam, 1 << LayerMask.NameToLayer(layer));
+    }
 
+    public static bool LayerCullingIncludes(this Camera cam, int layerMask)
+    {
+        return (cam.cullingMask & layerMask) > 0;
+    }
+
+    public static bool LayerCullingIncludes(this Camera cam, string layer)
+    {
+        return LayerCullingIncludes(cam, 1 << LayerMask.NameToLayer(layer));
+    }
+
+    public static void LayerCullingToggle(this Camera cam, int layerMask, bool isOn)
+    {
+        bool included = LayerCullingIncludes(cam, layerMask);
+        if (isOn && !included)
+        {
+            LayerCullingShow(cam, layerMask);
+        }
+        else if (!isOn && included)
+        {
+            LayerCullingHide(cam, layerMask);
+        }
+    }
+
+    public static void LayerCullingToggle(this Camera cam, string layer, bool isOn)
+    {
+        LayerCullingToggle(cam, 1 << LayerMask.NameToLayer(layer), isOn);
+    }
 
 }
