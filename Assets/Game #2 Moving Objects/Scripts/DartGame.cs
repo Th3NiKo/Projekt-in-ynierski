a46c292b3054ae12ca5aac882c1e6acd7671e069
janio95 <jankosiphone@gmail.com>
2018-12-20 11:18:34 +0200

    Dart

---
+++ Assets/Game #2 Moving Objects/Scripts/DartGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dart : MonoBehaviour {
+
+	bool hit;
+	Vector3 hitPosition;
+	void Start () {
+		hit = false;
+		hitPosition = Vector3.zero;
+	}
+	
+	
+	void Update () {
+		if(hit){
+			//this.transform.position = hitPosition;
+			this.transform.rotation = Quaternion.Euler(0,90,0);
+			this.GetComponent<Rigidbody>().isKinematic = true;
+		}
+	}
+	
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.name == "DartBoard")
+		{
+			hit = true;
+			hitPosition = this.transform.position;
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
+		if(other.gameObject.name == "Cursor"){
+			this.transform.rotation = Quaternion.Euler(0,90,0);
+		}
+	}
+}
